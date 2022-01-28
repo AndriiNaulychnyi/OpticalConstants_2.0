0ff101d1ffe68f46d0bc12b2b7a3946e308bcc90
AndriiNaulychnyi <andriy.naulychnyi@gmail.com>
2022-01-28 23:20:39 +0200

    updated field types

--- src/main/java/opticalconstants/model/IncomingData.java
+++ src/main/java/opticalconstants/model/IncomingData.java
@@ -3,15 +3,13 @@ package opticalconstants.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
 @Getter
 @Setter
 public class IncomingData {
     private Integer d;
-    private List<Double> lambda;
-    private List<Double> TFilm;
-    private List<Double> RFilm;
-    private List<Double> TSub;
-    private List<Double> RSub;
+    private Double lambda;
+    private Double TFilm;
+    private Double RFilm;
+    private Double TSub;
+    private Double RSub;
 }
