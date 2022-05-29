5e8c34746b212c665aed23b54cb87ab44c07c895
AndriiNaulychnyi <andriy.naulychnyi@gmail.com>
2022-05-29 19:11:55 +0300

    Update project

--- src/main/java/opticalconstants/controller/CalculationsController.java
+++ src/main/java/opticalconstants/controller/CalculationsController.java
@@ -3,7 +3,8 @@ package opticalconstants.controller;
 import opticalconstants.service.CalculationsService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,9 +21,13 @@ import java.io.IOException;
 @Controller
 public class CalculationsController {
     private static final Logger log = LoggerFactory.getLogger(CalculationsController.class);
-    private final String PATH_UPLOAD_FOLDER = "uploadFiles\\";
+    private final String PATH_UPLOAD_FOLDER = "tmp/input/";
 
     private final CalculationsService calculationService = new CalculationsService();
+
+    public CalculationsController() {
+        new File(PATH_UPLOAD_FOLDER).mkdirs();
+    }
 
     @RequestMapping(value = "/upload",
                     method = RequestMethod.POST,
@@ -34,8 +39,14 @@ public class CalculationsController {
         FileOutputStream fileOutputStream = new FileOutputStream(file);
         fileOutputStream.write(document.getBytes());
         fileOutputStream.close();
-        calculationService.calculation(d, file);
 
-        return new ResponseEntity<>("File is uploaded successfully", HttpStatus.OK);
+        var resultFile = calculationService.calculation(d, file);
+        var resource = new FileSystemResource(resultFile);
+
+
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + resource.getFilename() + "\"")
+                .body(resource);
     }
 }
--- src/main/java/opticalconstants/service/CalculationsService.java
+++ src/main/java/opticalconstants/service/CalculationsService.java
@@ -1,39 +1,44 @@
 package opticalconstants.service;
 
+import lombok.extern.slf4j.Slf4j;
 import opticalconstants.model.IncomingData;
 import opticalconstants.model.OutputData;
 import org.springframework.stereotype.Service;
-import lombok.extern.slf4j.Slf4j;
 
 import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.text.NumberFormat;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 @Service
 @Slf4j
 public class CalculationsService {
     private static final NumberFormat NUM_FORMAT = NumberFormat.getInstance(Locale.UK);
-    private final String PATH_DOWNLOAD_FOLDER = "D:\\IdeaProject\\OpticalConstants_2.0\\downloadFiles\\";
+    private final String PATH_DOWNLOAD_FOLDER = "tmp/results/";
 
     private final InputFileParser inputFileParser = new InputFileParser();
 
-    public List<OutputData> calculation(int d, File file) {
+    public CalculationsService() {
+        new File(PATH_DOWNLOAD_FOLDER).mkdirs();
+    }
+
+    public File calculation(int d, File file) {
         List<IncomingData> incomingDataList = inputFileParser.pars(d, file);
         List<CalculationResult> calculationResultList = calculationResult(incomingDataList);
         List<OutputData> outputDataList = getOutputDataList(calculationResultList);
-        File outputDataFile = createOutputDataFile(outputDataList);
-        System.out.println(" fssss");
-        return outputDataList;
+        return createOutputDataFile(outputDataList);
     }
 
     private List<CalculationResult> calculationResult(List<IncomingData> incomingDataList) {
         List<CalculationResult> calculationResultList = new ArrayList<>();
         for (IncomingData incomingData : incomingDataList) {
-            log.info("" + incomingData.getLambda());
+            log.info("{}", incomingData.getLambda());
             for (double k = 0; k <= 12; k+=0.01) {
                 for (double n = 0; n <= 6; n+=0.01) {
                     CalculationResult calculationResult= new CalculationResult(k, n, incomingData);
@@ -62,17 +67,18 @@ public class CalculationsService {
     }
 
     private File createOutputDataFile(List<OutputData> outputDataList) {
-        String fileName = getFileName();
-        File file = new File((PATH_DOWNLOAD_FOLDER + fileName));
+        var fileName = getFileName();
+        var file = new File(PATH_DOWNLOAD_FOLDER + fileName);
         try {
             file.createNewFile();
-            PrintWriter printWriter = new PrintWriter(file);
+            var printWriter = new PrintWriter(file);
             for (OutputData outputData : outputDataList) {
                 printWriter.println(getFormattedString(outputData));
             }
+            printWriter.flush();
             printWriter.close();
         } catch (IOException e) {
-            e.printStackTrace();
+            log.error("failed to save the data to result file '{}', error: {}", fileName, e);
         }
         return file;
     }
@@ -85,17 +91,19 @@ public class CalculationsService {
         int hour = calendar.get(Calendar.HOUR);
         int minute = calendar.get(Calendar.MINUTE);
         int second = calendar.get(Calendar.SECOND);
-        StringBuilder fileName = new StringBuilder("result_" + year + "_" + month + "_" + day + "_" + hour + "_" + minute + "_" + second + ".txt");
-        return fileName.toString();
+        return "result_" + year + "_" + month + "_" + day + "_" + hour + "_" + minute + "_" + second + ".txt";
     }
 
     private String getFormattedString(OutputData outputData) {
-        String lambda = BigDecimal.valueOf(outputData.getLambda()).setScale(4, BigDecimal.ROUND_HALF_UP).toString().replace ( '.', ',');
-        String tFilm = BigDecimal.valueOf(outputData.getTFilm()).setScale(4, BigDecimal.ROUND_HALF_UP).toString().replace ( '.', ',');
-        String rFilm = BigDecimal.valueOf(outputData.getRFilm()).setScale(4, BigDecimal.ROUND_HALF_UP).toString().replace ( '.', ',');
-        String n = BigDecimal.valueOf(outputData.getN()).setScale(4, BigDecimal.ROUND_HALF_UP).toString().replace ( '.', ',');
-        String k = BigDecimal.valueOf(outputData.getK()).setScale(4, BigDecimal.ROUND_HALF_UP).toString().replace ( '.', ',');
-        StringBuilder stringBuilder = new StringBuilder(lambda + "\t" + tFilm + '\t' + rFilm +'\t' + n + '\t' + k);
-        return stringBuilder.toString();
+        String lambda = BigDecimal.valueOf(outputData.getLambda()).setScale(4, RoundingMode.HALF_UP).toString().replace('.', ',');
+        String tFilm = BigDecimal.valueOf(outputData.getTFilm()).setScale(4, RoundingMode.HALF_UP).toString().replace('.', ',');
+        String rFilm = BigDecimal.valueOf(outputData.getRFilm()).setScale(4, RoundingMode.HALF_UP).toString().replace('.', ',');
+        String n = BigDecimal.valueOf(outputData.getN()).setScale(4, RoundingMode.HALF_UP).toString().replace('.', ',');
+        String k = BigDecimal.valueOf(outputData.getK()).setScale(4, RoundingMode.HALF_UP).toString().replace('.', ',');
+        return lambda + '\t' +
+                tFilm + '\t' +
+                rFilm + '\t' +
+                n + '\t' +
+                k;
     }
 }
